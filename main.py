bc240fdd58ab2335f065180bdc85eedd1c8de42f
DasKaroWow <daskarowow@gmail.com>
2025-07-16 02:15:08 +0300

    fix

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from app import create_app
 import logging
 
 app = create_app()
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
+logging.getLogger("werkzeug").disabled = True
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
